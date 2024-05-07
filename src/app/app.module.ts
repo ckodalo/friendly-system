ec6b66d640e0383fb6fc94ac53965dc11e5765a8
23po <ckodalo@gmail.com>
2024-05-07 14:46:12 +0300

    adds basic unstyled landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import {NgModule} from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { HeaderModule } from './components/header/header.module'
+import { FooterModule } from './components/footer/footer.module'
 import {AppComponent} from './app.component'
 import { LandingComponent } from './components/landing/landing.component'
 
 @NgModule({
     declarations: [
         AppComponent,
-        LandingComponent
+        LandingComponent,
     
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        HeaderModule,
+        FooterModule
     ],
     bootstrap: [AppComponent]
 })
