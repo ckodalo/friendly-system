354ad7fb1e5e190362e037a78d058976b2b8b127
23po <ckodalo@gmail.com>
2024-05-14 23:00:57 +0300

    import animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FooterModule } from './components/footer/footer.module'
 import { AppRoutingModule } from './app-routing.module'
 import { HttpClientModule } from '@angular/common/http'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { CommonModule } from '@angular/common'
 import {AppComponent} from './app.component'
 import { LandingComponent } from './components/landing/landing.component'
 import { AdminComponent } from './components/admin/admin.component'
@@ -12,12 +13,13 @@ import { TeacherComponent } from './components/teacher/teacher.component';
 import { StudentModalComponent } from './components/teacher/student-modal/student-modal.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts'
 import { BaseChartDirective } from 'ng2-charts'
 import { DashboardNavigationComponent } from './dashboard-navigation/dashboard-navigation.component'
 import { AssessmentsComponent } from './assessments/assessments.component'
 import { CreateAssessmentComponent } from './components/teacher/create-assessment/create-assessment.component'
-import { CommonModule } from '@angular/common'
+
 
 @NgModule({
     declarations: [
@@ -41,6 +43,7 @@ import { CommonModule } from '@angular/common'
         NgbModule, 
         FormsModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule,
         BaseChartDirective
     ],
     bootstrap: [AppComponent],
